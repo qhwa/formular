784146ddc49eda0a01117f42aba19a8efa358673
qhwa <qhwa@163.com>
2021-04-30 09:43:10 +0800

    bump version to v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Formular.MixProject do
   def project do
     [
       app: :formular,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
