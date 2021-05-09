485304d06457842fdcb19f00fd7f7ebe170dfe94
qhwa <qhwa@163.com>
2021-05-09 08:12:21 +0800

    pass linting

--- lib/formular.ex
+++ lib/formular.ex
@@ -114,6 +114,7 @@ defmodule Formular do
   end
 
   defp valid?(ast) do
+    # credo:disable-for-next-line 
     cond do
       contains_module_dot?(ast) ->
         {:error, :called_module_function}
