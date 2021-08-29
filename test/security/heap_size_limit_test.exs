27c7a7e9ac199423cf67dda0b3c12766bf0183d4
qhwa <qhwa@163.com>
2021-08-30 07:00:36 +0800

    feat: allow more functions

--- test/security/heap_size_limit_test.exs
+++ test/security/heap_size_limit_test.exs
@@ -4,14 +4,14 @@ defmodule HeapSizeLimitTest do
   defdelegate eval(code, binding, opts \\ []), to: Formular
 
   test "it works atom flooding" do
-    code = ~S|for a <- %Range{first: 0, last: 999_999_999_999}, do: :"#{a}"|
+    code = ~S|for a <- struct(Range, %{first: 0, last: 999_999_999_999, step: 1}), do: :"#{a}"|
 
-    assert eval(code, [], timeout: :infinity, max_heap_size: 1_000_000) == {:error, :killed}
+    assert eval(code, [], max_heap_size: 1_000_000) == {:error, :killed}
   end
 
   test "it works binary flooding" do
-    code = ~S|for a <- %Range{first: 0, last: 999_999_999_999}, do: "#{a}"|
+    code = ~S|for a <- 0..999_999_999_999, do: "#{a}"|
 
-    assert eval(code, [], timeout: :infinity, max_heap_size: 1_000_000) == {:error, :killed}
+    assert eval(code, [], max_heap_size: 1_000_000) == {:error, :killed}
   end
 end
