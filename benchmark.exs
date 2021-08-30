f6b5c0d3ca5f6482bed81b90a36c1933b4e3746a
qhwa <qhwa@163.com>
2021-08-30 22:18:58 +0800

    chore: update benchmark script

--- benchmark.exs
+++ benchmark.exs
@@ -1,13 +1,49 @@
 code = """
-  squares = %{3 => 9, 4 => 16, 5 => 25}
-  squares[3]
+for n <- args do
+  case n do
+    _ when is_integer(n) ->
+      n * n
+
+    _ ->
+      n
+  end
+end
 """
+
+defmodule Compiled do
+  def eval(binding) do
+    for n <- binding[:args] do
+      case n do
+        _ when is_integer(n) ->
+          n * n
+
+        _ ->
+          n
+      end
+    end
+  end
+end
+
+f = fn binding ->
+  for n <- binding[:args] do
+    case n do
+      _ when is_integer(n) ->
+        n * n
+
+      _ ->
+        n
+    end
+  end
+end
 
 ast = Code.string_to_quoted!(code)
 
 Benchee.run(%{
-  eval: fn -> {:ok, 9} = Formular.eval(code, []) end,
-  eval_ast: fn -> {:ok, 9} = Formular.eval(ast, []) end,
-  elixir_code_eval: fn -> {9, _} = Code.eval_string(code) end,
-  elixir_code_eval_ast: fn -> {9, _} = Code.eval_quoted(ast) end
+  eval: fn -> {:ok, [9]} = Formular.eval(code, args: [3]) end,
+  eval_ast: fn -> {:ok, [9]} = Formular.eval(ast, args: [3]) end,
+  elixir_code_eval: fn -> {[9], _} = Code.eval_string(code, args: [3]) end,
+  elixir_code_eval_ast: fn -> {[9], _} = Code.eval_quoted(ast, args: [3]) end,
+  compiled_module: fn -> [9] = Compiled.eval(args: [3]) end,
+  compiled_function: fn -> [9] = f.(args: [3]) end,
+  compiled_module_function: fn -> [9] = apply(Compiled, :eval, [[args: [3]]]) end
 })
